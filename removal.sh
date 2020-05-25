4812774bfae0485ddc1b67fbe5aed8559c777752
Sam <48739810+The-SamminAter@users.noreply.github.com>
2020-05-25 12:54:35 -0700

    Update removal.sh

--- removal.sh
+++ removal.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+#This is for removal/undoing any changes/things that any of the stages do.
+#I don't know how this'll work yet.
+#This is gonna rely on a file for info of the paths and other stuff.
