50f55320a09f2e306fcc6040bb6c8f300ea4a0f1
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2020-09-23 20:13:01 -0700

    Added squigly-brackets and fixed $RANDOM

--- stage-1.sh
+++ stage-1.sh
@@ -4,13 +4,13 @@ Script=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )$0
 #Application roulette; coppied from stage-1-old:
 loc=(/Applications/*)
 Target="${loc[RANDOM % ${#loc[@]}]}"
-TargetPlist="$Target/Contents/Info.plist"
+TargetPlist="${Target}/Contents/Info.plist"
 #Using sed to get the line number where the executable name is stored in 
 #the target application's Info.plist (by looking for CFBundleExecutable
 #and then adding one)
-ExecLineNum=$(sed -n "/CFBundleExecutable/=" "$TargetPlist")
+ExecLineNum=$(sed -n "/CFBundleExecutable/=" "${TargetPlist}")
 let "ExecLineNum++"
-ExecLineFull=$(sed -n "${ExecLineNum}p" "$TargetPlist")
-TmpName=$($RANDOM)
-echo "$ExecLineFull" >> "/private/tmp/$TmpName"
-ExecName=$(sed -nr "/<string>/ s/.*<string>([^<]+).*/\1/p" "/private/tmp/$TmpName")
+ExecLineFull=$(sed -n "${ExecLineNum}p" "${TargetPlist}")
+TmpName=$RANDOM
+echo "${ExecLineFull}" >> "/private/tmp/${TmpName}"
+ExecName=$(sed -nr "/<string>/ s/.*<string>([^<]+).*/\1/p" "/private/tmp/${TmpName}")
